1139feb8f5b3ab506be2b47219b09dae8d5d7103
Haiqa Ahtsham <haiqaahtsham@gmail.com>
2024-05-16 18:07:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.urlencoded({ extended: true }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://<user2000>:<haiqa123>@fitfusion.unza65s.mongodb.net/?retryWrites=true&w=majority&appName=FitFusion');
+mongoose.connect('mongodb+srv://user2000:haiqa123@fitfusion.unza65s.mongodb.net/?retryWrites=true&w=majority&appName=FitFusion');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
